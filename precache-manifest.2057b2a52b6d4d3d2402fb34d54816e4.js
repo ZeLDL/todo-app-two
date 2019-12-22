3e0cad0679bbc543920dedeacfab702187fefe3d
ZeLDL <31837563+ZeLDL@users.noreply.github.com>
2019-12-22 18:01:52 +0700

    Updates

---
+++ precache-manifest.2057b2a52b6d4d3d2402fb34d54816e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043162629a507c2c86c06428c57fa968",
+    "url": "/todo-app-two/index.html"
+  },
+  {
+    "revision": "6b3ed90f00dd469ad10c",
+    "url": "/todo-app-two/static/css/main.3fb8b239.chunk.css"
+  },
+  {
+    "revision": "14d8e3c069b401b5df78",
+    "url": "/todo-app-two/static/js/2.b0d1eb3f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-app-two/static/js/2.b0d1eb3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b3ed90f00dd469ad10c",
+    "url": "/todo-app-two/static/js/main.62059cd5.chunk.js"
+  },
+  {
+    "revision": "b873e9dca99cb2d7db24",
+    "url": "/todo-app-two/static/js/runtime-main.09b15d28.js"
+  }
+]);
